069cbe31b011bae70f8d99bb46bb48d4edea467e
saurav chennuri <saurav07@bu.edu>
2023-01-22 14:37:25 -0500

    modified for running Adaboost

--- runSeperate.sh
+++ runSeperate.sh
@@ -15,15 +15,67 @@ nohup bash shell_scripts/kTkV_SVR_all-metrics_100__RS_pearson_1.sh &
 #!/bin/sh
 nohup bash shell_scripts/kTkV_SVR_all-metrics_150__RS_pearson_1.sh &
 #!/bin/sh
-nohup bash shell_scripts/kTkV_SVR_all-metrics_2__stan_optimal_pearson_1.sh &
+nohup bash shell_scripts/kTkV_SVR_all-metrics_200__RS_pearson_1.sh &
 #!/bin/sh
-nohup bash shell_scripts/kTkV_SVR_all-metrics_4__stan_optimal_pearson_1.sh &
+nohup bash shell_scripts/kTkV_SVR_all-metrics_250__RS_pearson_1.sh &
 #!/bin/sh
-nohup bash shell_scripts/kTkV_SVR_all-metrics_9__stan_optimal_pearson_1.sh &
+nohup bash shell_scripts/kTkV_SVR_all-metrics_275__RS_pearson_1.sh &
 #!/bin/sh
-nohup bash shell_scripts/kTkV_SVR_all-metrics_13__stan_optimal_pearson_1.sh &
+nohup bash shell_scripts/kTkV_SVR_all-metrics_320__RS_pearson_1.sh &
 #!/bin/sh
-nohup bash shell_scripts/kTkV_SVR_all-metrics_17__stan_optimal_pearson_1.sh &
+nohup bash shell_scripts/kTkV_RF_all-metrics_10__RS_pearson_1.sh &
+#!/bin/sh
+nohup bash shell_scripts/kTkV_RF_all-metrics_20__RS_pearson_1.sh &
+#!/bin/sh
+nohup bash shell_scripts/kTkV_RF_all-metrics_25__RS_pearson_1.sh &
+#!/bin/sh
+nohup bash shell_scripts/kTkV_RF_all-metrics_40__RS_pearson_1.sh &
+#!/bin/sh
+nohup bash shell_scripts/kTkV_RF_all-metrics_50__RS_pearson_1.sh &
+#!/bin/sh
+nohup bash shell_scripts/kTkV_RF_all-metrics_80__RS_pearson_1.sh &
+#!/bin/sh
+nohup bash shell_scripts/kTkV_RF_all-metrics_100__RS_pearson_1.sh &
+#!/bin/sh
+nohup bash shell_scripts/kTkV_RF_all-metrics_150__RS_pearson_1.sh &
+#!/bin/sh
+nohup bash shell_scripts/kTkV_RF_all-metrics_200__RS_pearson_1.sh &
+#!/bin/sh
+nohup bash shell_scripts/kTkV_RF_all-metrics_250__RS_pearson_1.sh &
+#!/bin/sh
+nohup bash shell_scripts/kTkV_RF_all-metrics_275__RS_pearson_1.sh &
+#!/bin/sh
+nohup bash shell_scripts/kTkV_RF_all-metrics_320__RS_pearson_1.sh &
+#!/bin/sh
+nohup bash shell_scripts/kTkV_AdaBoost_all-metrics_10__RS_pearson_1.sh &
+#!/bin/sh
+nohup bash shell_scripts/kTkV_AdaBoost_all-metrics_20__RS_pearson_1.sh &
+#!/bin/sh
+nohup bash shell_scripts/kTkV_AdaBoost_all-metrics_25__RS_pearson_1.sh &
+#!/bin/sh
+nohup bash shell_scripts/kTkV_AdaBoost_all-metrics_40__RS_pearson_1.sh &
+#!/bin/sh
+nohup bash shell_scripts/kTkV_AdaBoost_all-metrics_50__RS_pearson_1.sh &
+#!/bin/sh
+nohup bash shell_scripts/kTkV_AdaBoost_all-metrics_80__RS_pearson_1.sh &
+#!/bin/sh
+nohup bash shell_scripts/kTkV_AdaBoost_all-metrics_100__RS_pearson_1.sh &
+#!/bin/sh
+nohup bash shell_scripts/kTkV_AdaBoost_all-metrics_150__RS_pearson_1.sh &
+#!/bin/sh
+nohup bash shell_scripts/kTkV_AdaBoost_all-metrics_200__RS_pearson_1.sh &
+#!/bin/sh
+nohup bash shell_scripts/kTkV_AdaBoost_all-metrics_250__RS_pearson_1.sh &
+#!/bin/sh
+nohup bash shell_scripts/kTkV_AdaBoost_all-metrics_275__RS_pearson_1.sh &
+#!/bin/sh
+nohup bash shell_scripts/kTkV_AdaBoost_all-metrics_320__RS_pearson_1.sh &
+#!/bin/sh
+nohup bash shell_scripts/kTkV_SVR_all-metrics_20__stan_optimal_pearson_1.sh &
+#!/bin/sh
+nohup bash shell_scripts/kTkV_RF_all-metrics_20__stan_optimal_pearson_1.sh &
+#!/bin/sh
+nohup bash shell_scripts/kTkV_AdaBoost_all-metrics_20__stan_optimal_pearson_1.sh &
 #!/bin/sh
 nohup bash shell_scripts/kTkV_SVR_all-metrics_5__LS_pearson_1.sh &
 #!/bin/sh
@@ -40,6 +92,38 @@ nohup bash shell_scripts/kTkV_SVR_all-metrics_50__LS_pearson_1.sh &
 nohup bash shell_scripts/kTkV_SVR_all-metrics_80__LS_pearson_1.sh &
 #!/bin/sh
 nohup bash shell_scripts/kTkV_SVR_all-metrics_100__LS_pearson_1.sh &
+#!/bin/sh
+nohup bash shell_scripts/kTkV_RF_all-metrics_5__LS_pearson_1.sh &
+#!/bin/sh
+nohup bash shell_scripts/kTkV_RF_all-metrics_10__LS_pearson_1.sh &
+#!/bin/sh
+nohup bash shell_scripts/kTkV_RF_all-metrics_20__LS_pearson_1.sh &
+#!/bin/sh
+nohup bash shell_scripts/kTkV_RF_all-metrics_25__LS_pearson_1.sh &
+#!/bin/sh
+nohup bash shell_scripts/kTkV_RF_all-metrics_40__LS_pearson_1.sh &
+#!/bin/sh
+nohup bash shell_scripts/kTkV_RF_all-metrics_50__LS_pearson_1.sh &
+#!/bin/sh
+nohup bash shell_scripts/kTkV_RF_all-metrics_80__LS_pearson_1.sh &
+#!/bin/sh
+nohup bash shell_scripts/kTkV_RF_all-metrics_100__LS_pearson_1.sh &
+#!/bin/sh
+nohup bash shell_scripts/kTkV_AdaBoost_all-metrics_5__LS_pearson_1.sh &
+#!/bin/sh
+nohup bash shell_scripts/kTkV_AdaBoost_all-metrics_10__LS_pearson_1.sh &
+#!/bin/sh
+nohup bash shell_scripts/kTkV_AdaBoost_all-metrics_20__LS_pearson_1.sh &
+#!/bin/sh
+nohup bash shell_scripts/kTkV_AdaBoost_all-metrics_25__LS_pearson_1.sh &
+#!/bin/sh
+nohup bash shell_scripts/kTkV_AdaBoost_all-metrics_40__LS_pearson_1.sh &
+#!/bin/sh
+nohup bash shell_scripts/kTkV_AdaBoost_all-metrics_50__LS_pearson_1.sh &
+#!/bin/sh
+nohup bash shell_scripts/kTkV_AdaBoost_all-metrics_80__LS_pearson_1.sh &
+#!/bin/sh
+nohup bash shell_scripts/kTkV_AdaBoost_all-metrics_100__LS_pearson_1.sh &
 wait
 python3 writeFile.py
 python3 scripts/combineOutputs.py
