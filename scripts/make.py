0b218352a9a679d572e472bdbe7f6082eeb76c17
Saurav vara prasad Chennuri <saurav@crc-dot1x-nat-10-239-116-62.bu.edu>
2023-01-24 14:24:25 -0500

    makes shell scripts for different modalities and their combinations

--- scripts/make.py
+++ scripts/make.py
@@ -2,19 +2,26 @@ import numpy as np
 import itertools 
 import os 
 import argparse
+import pathlib
+
+"""make scripts that run in parallel. Makes it faster to do the work.
+
+Returns: shell scripts
+    _type_: .sh files in ../shell_scripts
+"""
 
 
-PATH =  "/projectnb/skiran/saurav/Fall-2022/src2/"
+PATH =  "/Users/saurav/Desktop/Margrit/Fall-22/WAB-prediction/"
 
-datasets = ["RS", "FA", "WM", "GM", "LS", "DM", "BH", "stan_optimal"]
+datasets = ["RS", "FA", "PSW", "PSG", "LS", "DM", "stan_optimal"]
 
 parameters = {
                 "-CV" : ["kTkV"], #["LOO", "LFiveO", "kTkV"],
-                "-model" : ["SVR"],#["RF", "SVR"], #["RF","SVR"], #["RF", "SVR"],
+                "-model" : ["SVR", "RF"],#["RF", "SVR"], #["RF","SVR"], #["RF", "SVR"],
                 "-metric" : ["all-metrics"],
                 "-f" :  None,#[5, 10,20,25,40,50,80,100,150,200,250,275,320,370,400,500,600,800,1000,1127], #[10],#,20,25,40,60,80,105], #[5, 10,20,25,40,50,80,100,150,200,250,275,320,370,400,500,600,800,1000,1127]
                 "-stratified":  [''],
-                "-data" : [ "RS", "stan_optimal", "LS"],
+                "-data" : None, #[ "RS", "stan_optimal", "LS"],
                 "-features_R" : ["pearson"],#["pearson", "RFE"],
                 "-frstep" : [1]
                 #['True', ''], # [True, ''],
@@ -29,11 +36,17 @@ features = {
             "RS" : [10,20,25,40,50,80,100,150],#,200,250,275,320,370,400,500,600,800,1000],
             "LS" : [5,10,20,25,40,50,80,100],
             "MM" : [3],
+            
+            "FA" : [2,4,6,8,10,12],
+            "PSW" : [2,4,8,12,16,20,25,32,36],
+            "PSG" : [2,4,8,16,25,32,40,48,55,69],
+            "LS" : [1],
+            "DM" : [1,2,3]
             }
 
 def get_dataset_combinations():
     all_combinations = []
-    for i in range(1,len(datasets)+1):
+    for i in range(2,len(datasets)+1):
         temp = list( map(list, list(itertools.combinations(datasets, i))))
         all_combinations += temp 
 
@@ -49,37 +62,42 @@ def get_different_combinations():
     total_combs = []
     for source in parameters["-data"]:
         parameters["-data"] = [source] 
-        parameters["-f"] = features[source]
+        if source in features:
+            parameters["-f"] = features[source]
+        else:
+            parameters["-f"] = [-1]
         combinations = list(itertools.product(*parameters.values()))
         total_combs += combinations
     return total_combs 
+
+
+        
+def write_scripts(args, level, f2):
     
-
-if __name__ == "__main__":
-
-        combinations_datasets = get_dataset_combinations()
-        parameters["-data"] = combinations_datasets
-
-        parser = argparse.ArgumentParser(description="enter makeFile Arguments")
-        parser.add_argument("-d", type = str, help = "Modality : [ runSeperate, runCombined ] ")
-        # parser.add_argument("-o", type = str, help = "Enter output file name")
-        args = parser.parse_args()
     
+            
+
+        if level == "level_1":
+            parameters["-data"] = datasets 
+
+        elif level == "level_2":
+            combinations_datasets = get_dataset_combinations()
+            parameters["-data"] = combinations_datasets
+
         if args.d == "runCombined":
             parameters["-data"] = ["MM"]
-            
-        combinations = get_different_combinations()
-        
 
-        runFname = args.d
-        f2 = open(PATH + runFname +".sh", "w")
-        
+        combinations = get_different_combinations()
+            
+        # pathlib.Path(PATH ).mkdir(parents=True, exist_ok=True)
+    
         param_ids = list(parameters)
         
         for i,c in enumerate(combinations):
             # print("c = ", c)
             # print(c[0], c[1], c[2], c[3])
             path =  PATH + "shell_scripts/" 
+            pathlib.Path(path).mkdir(parents=True, exist_ok=True)
             fname = ""
             for j,p in enumerate(c):
                 if j != len(c)-1:
@@ -89,7 +107,12 @@ if __name__ == "__main__":
             
             print("file name = ", fname, "\n")
             # fname =  c[0] + "_" + c[1] + "_" + c[2] + "_" + str(c[3]) + ".sh"
-            f = open(path + fname, "w")
+            if level == "level_1":
+                pathlib.Path(path + "seperate-datasets/").mkdir(parents=True, exist_ok=True)
+                f = open(path + "seperate-datasets/" + fname, "w")
+            elif level == "level_2":
+                pathlib.Path(path + "dataset-combinations/").mkdir(parents=True, exist_ok=True)
+                f = open(path + "dataset-combinations/" + fname, "w")
             f.write("#!/bin/sh" + "\n")
             f2.write("#!/bin/sh" + "\n")
             
@@ -108,27 +131,47 @@ if __name__ == "__main__":
             # f.write("python3 " + "scripts/main.py -CV "+ str(c[0]) + " -model " + str(c[1]) 
             #         + " -metric " + str(c[2]) + " -f" + str(c[3]) +
             #         " -same_split False")
-            
+            str_ = "nohup bash shell_scripts/" + "seperate-datasets/" if level == "level_1" else "nohup bash shell_scripts/" + "dataset-combinations/"             
+
             if i != len(combinations)-1:
-                    f2.write("nohup bash shell_scripts/" + fname + " &" + "\n")
+                    f2.write(str_ + fname + " &" + "\n")
             else:
-                    f2.write("nohup bash shell_scripts/" + fname + " &" + "\n")
-                    f2.write("wait" + "\n")
+                    f2.write(str_ + fname + " &" + "\n")
+                    # f2.write("wait" + "\n")
 
             f.close()
-
-            os.system("chmod +x " + path + fname)
-            #  os.system("./" + fname)
-        
+            
+            if level == "level_1":
+                os.system("chmod +x " + path + "seperate-datasets/" + fname)
+            elif level == "level_2":
+                os.system("chmod +x " + path + "dataset-combinations/" + fname)
+                        
         os.system("chmod +x " + PATH + runFname +".sh")
         
-        #  write both below lines in second script 
-        if args.d != "MM":
-            
-            f2.write("python3 " + "writeFile.py" + "\n")
-            f2.write("python3 " + "scripts/combineOutputs.py")
-        else:
-            f2.write("python3 " + "writeFile.py")
-        f2.close()
-        
-      
+ 
+if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser(description="enter makeFile Arguments")
+    parser.add_argument("-d", type = str, help = "Modality : [ runSeperate, runCombined ] ")
+    # parser.add_argument("-o", type = str, help = "Enter output file name")
+    args = parser.parse_args()
+
+    runFname = args.d
+
+    f2 = open(PATH + runFname +".sh", "w")
+
+    write_scripts(args, "level_1", f2)  # creates scripts for seperate dataset modalities to run in "shell_scripts/seperate-datasets"
+    # write_scripts(args, "level_2", f2)
+
+    if args.d != "MM": 
+        f2.write("python3 " + "writeFile.py" + "\n")
+        f2.write("python3 " + "scripts/combineOutputs.py")
+    else:
+        f2.write("python3 " + "writeFile.py")
+
+    f2.close()
+
+    os.system("chmod +x " + PATH + runFname +".sh")
+
+
+     
