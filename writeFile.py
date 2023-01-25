a5f81d75e05fe825b9b7320226fc71a3fd165172
saurav chennuri <saurav07@bu.edu>
2023-01-25 12:55:13 -0500

    saves outputs of all combinations of modalities

--- writeFile.py
+++ writeFile.py
@@ -13,6 +13,7 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.decomposition import PCA
 from sklearn.model_selection import KFold
 from sklearn.metrics import mean_squared_error, mean_absolute_error
+from scripts.make import *
 
 from sklearn.svm import SVR
 import itertools
@@ -23,25 +24,30 @@ PATH = "/projectnb/skiran/saurav/Fall-2022/src2/"
 # files = [f for f in listdir(PATH + "results/") if isfile(join(PATH + "results/", f))]
 
 results_folder = PATH + "results/model_performances"
-output_file =  PATH + "results/final.csv"
+output_file_path = PATH + "results/"
+# output_file =  PATH + "results/final.csv"
 
 
 num_ranks = 3
 
-def writeResults():
+def writeResults(predictionModel):
+    """Writes the top-3 best prediction outputs in each of the top-k feature setting 
+    
+    """
+    output_file = output_file_path + predictionModel + "_final.csv"
     with open(output_file,"w", newline='') as csvfile:
         writer = csv.writer(csvfile, delimiter=',',
                                 quotechar='|', quoting=csv.QUOTE_MINIMAL)
         writer.writerow(["CV type", "model", "metrics", "num features", "reduction method","stratified","data source", "train RMSE", "validate RMSE", "test RMSE",
                                                                         "train RMSE rank", "validate RMSE rank", "test RMSE rank",
                                                                         "train MAE", "validate MAE", "test MAE",
-                                                                        "train MAE rank", "validate MAE rank", "test MAE rank", "model parameters", "PARAM1", "PARAM2", "PARAM3", "PARAM4", "PARAM5"])
+                                                                        "train MAE rank", "validate MAE rank", "test MAE rank", "SupportVectors", "model parameters", "PARAM1", "PARAM2", "PARAM3", "PARAM4"])
                                                                         
         files = os.listdir(PATH + "results")
         dataFolders = [f for f in files if not os.path.isfile(PATH + "results/" + f)]
     
         for dataFolder in dataFolders:
-            results_folder = PATH + "results/" + dataFolder + "/model_performances"
+            results_folder = PATH + "results/" + dataFolder + "/" + predictionModel + "_predictions" + "/model_performances"
             files = [f for f in listdir(results_folder + "/") if isfile(join(results_folder + "/", f))]
     
             for file in files:
@@ -66,7 +72,7 @@ def writeResults():
                     validate_MAE, validate_MAE_rank = data_subset.iloc[i]["validate performance MAE"] if "validate performance MAE" in data_subset.columns else "-", data_subset.iloc[i]["validate rank"] if "validate rank" in data_subset.columns else data_subset.iloc[i]["validate MAE rank"] if "validate MAE rank" in data_subset.columns else "-"
                     
                     # print("cols = ", data_subset.columns)
-                    support_vectors = data_subset.iloc[i]["support vectors"]
+                    support_vectors = str(data_subset.iloc[i]["support vectors"])
                     
                     params = data_subset.iloc[i][param_id]
                     param_dict = dict(zip(param_id.split("|"), params.split(",")))
@@ -77,29 +83,37 @@ def writeResults():
                         writer.writerow([train_settings[0], train_settings[1], train_settings[2], train_settings[3], train_settings[-1][:-4], train_settings[4],dataFolder, train_MSE, validate_MSE, test_MSE,
                                                                                                                     train_MSE_rank, validate_MSE_rank, test_MSE_rank,
                                                                                                                     train_MAE, validate_MAE, test_MAE,
-                                                                                                                    train_MAE_rank, validate_MAE_rank, test_MAE_rank, param_dict, support_vectors])
+                                                                                                                    train_MAE_rank, validate_MAE_rank, test_MAE_rank, support_vectors, param_dict])
                     else:
                         writer.writerow([train_settings[0], train_settings[1], train_settings[2], train_settings[3], train_settings[-1][:-4], train_settings[4], dataFolder, train_MSE, validate_MSE, test_MSE,
                                                                                                                     train_MSE_rank, validate_MSE_rank, test_MSE_rank,
                                                                                                                     train_MAE, validate_MAE, test_MAE,
-                                                                                                                    train_MAE_rank, validate_MAE_rank, test_MAE_rank, param_dict, support_vectors ])
+                                                                                                                    train_MAE_rank, validate_MAE_rank, test_MAE_rank,support_vectors, param_dict])
     
 
-def writeFiles():
-    
+def writeFiles(predictionModel):
     files = os.listdir(PATH + "results")
     dataFolders = [f for f in files if not os.path.isfile(PATH + "results/" + f)]    
+    output_file = output_file_path + predictionModel + "_final.csv"
     totalData = pd.read_csv(output_file)     
-    
     for source in dataFolders:
         data = totalData[totalData["data source"] == source]
-        data.to_csv(PATH+"results/" + source + "/" + source + "_aggregate.csv")
-    
+        data = data[data["model"] == predictionModel]
+        data.to_csv(PATH+"results/" + source + "/"+ predictionModel + "_predictions"+ "/"+ source + "_aggregate.csv")
         
+        
+def modelWiseUpdate(predictionModels):
+    for predictionModel in predictionModels:
+        writeResults(predictionModel)
+        writeFiles(predictionModel)
+    
     
 if __name__ == "__main__":
-    writeResults()
-    writeFiles()
+    predictionModels = parameters["-model"]
+    modelWiseUpdate(predictionModels)
+    
+    
+    
     
     
     
