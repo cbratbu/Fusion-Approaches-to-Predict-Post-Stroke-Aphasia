abdbe69d71c532c6ebf9e9d472222fa20e530595
saurav chennuri <saurav07@bu.edu>
2023-01-26 11:07:22 -0500

    creates level2 data for LateFusion in path 'data/LateFusion/model/'

--- scripts/combineOutputs.py
+++ scripts/combineOutputs.py
@@ -2,13 +2,15 @@ import pandas as pd
 import re
 import os 
 from make import *
+import argparse
 # from params import *
 
 final_path = "/projectnb/skiran/saurav/Fall-2022/src2/results/"
 data_path = "/projectnb/skiran/saurav/Fall-2022/src2/data/"
 
-def getBestParams(dataSource, predictionModel):
-    output_file = final_path + dataSource + "/" + predictionModel + "_predictions" + "/" + dataSource + "_aggregate.csv"
+def getBestParams(dataSource, predictionModel, level, approach):
+    output_file = final_path + approach + "/" + predictionModel + "_predictions" + "/" + level  + "/" +  dataSource + "/"  + dataSource + "_aggregate.csv"
+    # print("output file = ", output_file)
     data = pd.read_csv(output_file,  delimiter=",")
     #
     data = data.sort_values(by = "validate RMSE")
@@ -50,9 +52,9 @@ def getBestParams(dataSource, predictionModel):
     fname = fname[:-1] + ".csv"
     return fname
 
-def getBestModel(dataSource, predictionModel):
+def getBestModel(dataSource, predictionModel, level, approach):
     best_model = ""
-    data = pd.read_csv(final_path + dataSource + "/" + predictionModel + "_predictions" +"/"+ dataSource+"_aggregate.csv")
+    data = pd.read_csv(final_path + approach + "/" + predictionModel + "_predictions" +"/" + level + "/" + dataSource + "/" + dataSource+"_aggregate.csv")
     data = data.sort_values(by = "validate RMSE")
     data = data[data["model"] == predictionModel]
     model_params = data.iloc[0,1:6]
@@ -64,21 +66,25 @@ def getBestModel(dataSource, predictionModel):
     return best_model
 
 
-def getOutputFname(dataSources, predictionModel):
-    os.makedirs(data_path + "lateFusionData/" + predictionModel + "/", exist_ok = True)
+def getOutputFname(dataSources, predictionModel, approach, level):
+    if approach == "LF":
+        os.makedirs(data_path + "lateFusionData/" + predictionModel + "/", exist_ok = True)
+    elif approach == "EF":
+        os.makedirs(data_path + "earlyFusionData/" + predictionModel + "/", exist_ok = True)
+        
     if len(dataSources) < len(datasets):
         dataSources = sorted(dataSources)
         fname = ""
         for dataSource in dataSources:
             fname += dataSource.split("_")[0] + "_"
         fname += "ModalityOutputs.xlsx"
-        return data_path + "lateFusionData/"+ predictionModel + "/" + fname
+        return data_path + "lateFusionData/"+ predictionModel + "/" + predictionModel + "_" +  fname
             
     else:
         return data_path + "lateFusionData/"+ predictionModel + "/" + predictionModel + "_" + "allModalityOutputs.xlsx"
             
 
-def organizeOutputData(dataPaths, sources, predictionModel):
+def organizeOutputData(dataPaths, sources, predictionModel, approach, level):
     allModalityPreds = dict()
     # print("size of dataPaths = ", len(dataPaths))
     for i,bestM in enumerate(dataPaths):
@@ -88,38 +94,47 @@ def organizeOutputData(dataPaths, sources, predictionModel):
         allModalityPreds[sources[i]] = predictions
     allModalityPreds["ground truth score"] = ground_truths
     allModalityPreds = pd.DataFrame(allModalityPreds)
-    outputFname = getOutputFname(sources, predictionModel)
-    allModalityPreds.to_excel(outputFname)  # update this path based on what kind of fusion approach you are trying
+    outputFname = getOutputFname(sources, predictionModel, approach, level)
+    allModalityPreds.to_excel( outputFname)  # update this path based on what kind of fusion approach you are trying
                                                                                                                 # also update if any data combinations you are going to try here.
         
         
-def saveBestFiles(predictionModel, dataSources):
+def saveBestFiles(predictionModel, dataSources, level):
     datapaths = []
-    for source in dataSources:
-        bestParams = getBestParams(source, predictionModel) # gets you the best training parameters for a dataSource with certain prediction model
-        bestModel = getBestModel(source, predictionModel) # need to update what this does. 
-        datapaths.append(final_path + source + "/" + predictionModel + "_predictions"+ "/outputs/" + bestModel + "/" + bestParams)
-    organizeOutputData(datapaths, dataSources, predictionModel)
+    approaches = parameters["-approach"]
+    for approach in approaches:
+        for source in dataSources:
+            bestParams = getBestParams(source, predictionModel, level, approach) # gets you the best training parameters for a dataSource with certain prediction model
+            bestModel = getBestModel(source, predictionModel, level, approach) # need to update what this does. 
+            datapaths.append(final_path + approach + "/" + predictionModel + "_predictions/" + level + "/" + source + "/"  + "outputs/" + bestModel + "/" + bestParams)
+    organizeOutputData(datapaths, dataSources, predictionModel, approach, level)
     
         
-def saveBestOutputs_dataSourceCombinations(dataSources, predictionModels):  # saves outputs combinations for different data source combinations
+def saveBestOutputs_dataSourceCombinations(dataSources, predictionModels, level):  # saves outputs combinations for different data source combinations
     dataSourcesCombinations = []
-    for i in range(2,len(dataSources)):
+    for i in range(1,len(dataSources)+1):
         dataSourcesCombinations += list(map(list,list(itertools.combinations(dataSources, i))))
         
     for dataSources in dataSourcesCombinations:
         for predictionModel in predictionModels:
-            saveBestFiles(predictionModel, dataSources)
+            saveBestFiles(predictionModel, dataSources, level)
 
 if __name__ == "__main__":
-    files = os.listdir(final_path)
-    dataSources = [f for f in files if not os.path.isfile(final_path + f)]    
+    parser = argparse.ArgumentParser(description="enter combineOutputs Arguments")
+    parser.add_argument("-level", type = str, help = "levels : [ level1, level2 ] ", default = "level1")
+    # parser.add_argument("-o", type = str, help = "Enter output file name")
+    args = parser.parse_args()
+    level = args.level
+    approaches = parameters["-approach"]
+
+    files = datasets
+    dataSources = [f.split("_")[0] + "_results" for f in files if not os.path.isfile(final_path + f)] 
     predictionModels = parameters["-model"]
 
     # for predictionModel in predictionModels:
     #     saveBestFiles(predictionModel, dataSources)
         
-    saveBestOutputs_dataSourceCombinations(dataSources, predictionModels)
+    saveBestOutputs_dataSourceCombinations(dataSources, predictionModels, level)
     
     
     
