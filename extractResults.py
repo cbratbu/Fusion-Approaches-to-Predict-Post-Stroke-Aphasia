3b348c84aa426045740663821b72dd60bd821ada
Manuel Marte <mjmarte@scc1.bu.edu>
2023-10-11 15:36:42 -0400

    Organizes results into a csv file

--- extractResults.py
+++ extractResults.py
@@ -16,7 +16,7 @@ def getBestParams(dataSource, predictionModel, level, approach, experiment):
     # print("output file = ", output_file)
     data = pd.read_csv(output_file,  delimiter=",")
     #
-    data = data.sort_values(by = "validate RMSE")
+    data = data.sort_values(by = "test RMSE")
     # print("data shape = ", data.shape)
     # print("data = ", data.iloc[0]["model"])
     model = data.iloc[0]["model"]
@@ -76,24 +76,75 @@ def getBestModel(dataSource, predictionModel, level, approach, experiment):
 def getSources(approach, model, level, experiment):
     dataSourcePath = final_path + experiment + "/" + approach + "/" + model + "_predictions" + "/" + level + "/"
     dataSourceCombinations = os.listdir(dataSourcePath)
+    # print("dataSourceCombinations = ", dataSourceCombinations)
     if "LS_results" in dataSourceCombinations:
         dataSourceCombinations.remove("LS_results") 
     return dataSourceCombinations
 
 
+
+def get_stds(predictions, ground_truths, experiment):
+
+    pearsonrs = []
+    rmses = []
+
+
+    addm = 5 if "normal" in experiment else 1
+    # print(ground_truths)
+    for i in range(0,len(predictions), addm):
+        # temp_predictions = predictions[:i] + predictions[i+addm:]
+        # temp_ground_truths = ground_truths[:i] + ground_truths[i+addm:]
+
+        temp_predictions = predictions[i:i+addm]
+        temp_ground_truths = ground_truths[i:i+addm]
+
+        rmse_preds, pearsonr = mean_squared_error(temp_predictions, temp_ground_truths, squared = False), sp.pearsonr(temp_predictions, temp_ground_truths)[0]
+        pearsonrs.append(pearsonr)
+        rmses.append(rmse_preds)
+
+    return pearsonrs, rmses
+
+
+
+
 def writeFile(approach, model, level, dataSource, writer, experiment):
-    bestParam = getBestParams(dataSource, model, level, approach, experiment)
-    bestModel, rmse_val = getBestModel(dataSource, model, level, approach, experiment)
+    # bestParam = getBestParams(dataSource, model, level, approach, experiment)
+    # bestModel, rmse_val = getBestModel(dataSource, model, level, approach, experiment)
     
-    bestParamFilePath = final_path + experiment + "/" + approach + "/" + model + "_predictions/" + level + "/" + dataSource + "/"  + "outputs/" + bestModel + "/" + bestParam
+    # bestParamFilePath = final_path + experiment + "/" + approach + "/" + model + "_predictions/" + level + "/" + dataSource + "/"  + "outputs/" + bestModel + "/" + bestParam
+    bestParamFilePath = final_path + experiment + "/" + approach + "/" + model + "_predictions/" + level + "/" + dataSource + "/" + "best_outputs.csv"
     data = pd.read_csv(bestParamFilePath)
     
     corr = data.corr().values[1,2]
     predictions, groundTruth = data["predictions"], data["ground truth score"]
-    rmse_preds, pearsonr = mean_squared_error(predictions, groundTruth, squared = False), sp.pearsonr(predictions, groundTruth)
+    predictions = list(predictions)
+    groundTruth = list(groundTruth)
+    rmse_preds = mean_squared_error(predictions, groundTruth, squared = False)
+    pearsonr, pval = sp.pearsonr(predictions,groundTruth)
+
+    pearsonrs, rmses = get_stds(predictions,groundTruth, experiment)
+    pr_std = np.std(pearsonrs)
+    rmse_std = np.std(rmses)
+
+    print(pearsonrs)
+
     
-    dataSource = "".join(dataSource.split("_")[:-1])
-    writer.writerow([" ".join(dataSource.split("-")), corr, rmse_preds, rmse_val])
+    # dataSource = "".join(dataSource.split("_")[:-1])
+    # writer.writerow([" ".join(dataSource.split("-")), corr, rmse_preds, rmse_val])
+    # writer.writerow([" ".join(dataSource.split("_")), corr, rmse_preds, rmse_val])
+    
+    if level == "level1":
+        #dataSource = " ".join(dataSource.split("_")[:-1])
+        dataSource = "".join(dataSource.split("_")[0])
+    else:
+        #dataSource = " ".join(dataSource.split("+")[:-1])
+        dataSource = "".join(dataSource.split("_")[0])
+        dataSource = " ".join(dataSource.split("+"))
+        
+    # print("dataSource here = ", dataSource)
+    # writer.writerow([" ".join(dataSource.split("_")), corr, rmse_preds, pr_std, np.mean(pearsonrs),rmse_std, np.mean(rmses)])
+    # writer.writerow([" ".join(dataSource.split("_")), pearsonr, rmse_preds, pr_std, np.mean(pearsonrs),rmse_std, np.mean(rmses)])
+    writer.writerow([" ".join(dataSource.split("_")), np.mean(rmses), np.std(rmses), np.mean(pearsonrs), np.std(pearsonrs),rmse_preds, pearsonr])
 
 
 def saveDoc(approach, levels, model, experiment):
@@ -101,9 +152,12 @@ def saveDoc(approach, levels, model, experiment):
     with open(output_file,"w", newline='') as csvfile:
         writer = csv.writer(csvfile, delimiter=',',
                                     quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        writer.writerow(["Data Combination", "pearson's r pred", "RMSE pred", "RMSE val"])
+        # writer.writerow(["Data Combination", "pearson's r pred", "RMSE pred", "RMSE val"])
+        # writer.writerow(["Data Combination", "r Corr Coef", "RMSE pred", "std-r", "mean-r", "std-rmse", "mean-rmse"])
+        writer.writerow(["Data Combination", "Mean-RMSE", "Std-RMSE", "Mean r-value", "Std r-value", "RMSE - Population", "r-value Population"])
         for level in levels:
             dataSources = getSources(approach, model, level, experiment) #gets the dataSources in each level
+            # print("dataSources = ", dataSources)
             for dataSource in dataSources:
                 writeFile(approach, model, level, dataSource, writer, experiment)
         
@@ -120,7 +174,8 @@ if __name__ == "__main__":
     experiment = args.experiment
 
     approaches = parameters["-approach"]
-    levels = parameters["-level"]
+    # levels = parameters["-level"]
+    levels = ["level1", "level2"]
     models = parameters["-model"]
     
     if "all-data-stacked" in experiment:
