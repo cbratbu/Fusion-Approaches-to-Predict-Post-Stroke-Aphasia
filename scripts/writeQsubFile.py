145e354ecddee29f26bacbd49c0f85f7d68ba3ad
Manuel Marte <mjmarte@scc1.bu.edu>
2023-10-11 15:24:25 -0400

    Seperates LSF job shell scripts based on #experiments

--- scripts/writeQsubFile.py
+++ scripts/writeQsubFile.py
@@ -16,6 +16,7 @@ def writeTestShellFile(file, experiment=None, runType=None):
         
         file.write("#$ -l h_rt=70:00:00" + "\n")
         file.write("#$ -m beas" + "\n\n")
+        file.write("#$ -M saurav07@bu.edu" + "\n\n")
         
         file.write("module load python3/3.8.10" + "\n")
         file.write("module load pytorch/1.11.0" + "\n\n")
@@ -23,7 +24,8 @@ def writeTestShellFile(file, experiment=None, runType=None):
         # file.write("./make.sh" + "\n\n")
     
     if experiment != None: 
-        file.write("./" + "experiment_scripts/" + experiment + "/" + runType + ".sh" + "\n")
+        # file.write("./" + "experiment_scripts/" + experiment + "/" + runType + ".sh" + "\n")
+        file.write(PATH + "experiment_scripts/" + experiment + "/" + runType + ".sh" + "\n")
         if runType == "runSeperate":
             file.write("wait" + "\n")
         
@@ -31,13 +33,74 @@ def writeTestShellFile(file, experiment=None, runType=None):
 if __name__ == "__main__":
 
     for experiment in experiments:
-        file = open(PATH + experiment + ".sh", "w")
         
-        writeTestShellFile(file)
-        writeTestShellFile(file, experiment, "runSeperate")
-        writeTestShellFile(file, experiment, "runCombined")
+        rs = PATH + "experiment_scripts/" + experiment + "/" + "runSeperate" +".sh"
+        with open(rs, 'r') as file:
+            rslines = len(file.readlines())
         
-        os.system("chmod +x " + PATH + experiment + ".sh")
+        rsnumfs = int(rslines/20)+2
+        
+        cs = PATH + "experiment_scripts/" + experiment + "/" + "runCombined" +".sh"
+        with open(cs, 'r') as file:
+            cslines = len(file.readlines())
+        csnumfs = int(cslines/20)+2
+        
+        
+        os.makedirs(PATH + "experiment_jobs", exist_ok=True)
+        os.makedirs(PATH + "submit_jobs", exist_ok=True)
+        os.makedirs(PATH + "submit_jobs/" + experiment, exist_ok=True)
+        
+        os.makedirs(PATH + "experiment_jobs/" + experiment + "/" + "seperate_jobs", exist_ok=True)
+        os.makedirs(PATH + "experiment_jobs/" + experiment + "/" + "combined_jobs", exist_ok=True)
+        
+        
+        file2 = open(PATH + "submit_jobs/" + experiment + "/" + "seperate_jobs.sh", "w")
+        for i in range(1,rsnumfs, 1):
+            file = open(PATH + "experiment_jobs/" + experiment + "/" + "seperate_jobs/" + experiment + "_" + str(i) + ".sh", "w")
+            writeTestShellFile(file)
+            writeTestShellFile(file, experiment, "runSeperate_" + str(i))
+            os.system("chmod +x " + PATH + "experiment_jobs/" + experiment + "/" + "seperate_jobs/" + experiment + "_" + str(i) + ".sh")
+            
+            if i != rsnumfs-1:
+                file2.write("qsub " + PATH + "experiment_jobs/" + experiment + "/" + "seperate_jobs/" + experiment + "_" + str(i) + ".sh" + "\n")
+            else:
+                # file2.write("wait" + "\n")
+                file2.write("qsub " + PATH + "experiment_jobs/" + experiment + "/" + "seperate_jobs/" + experiment + "_" + str(i) + ".sh" + "\n")
+                
+        file2.close()
+            
+                
+            
+            
+            
+        file2 = open(PATH + "submit_jobs/" + experiment + "/" + "combined_jobs.sh", "w")
+            
+        for i in range(1,csnumfs, 1):
+            file = open(PATH + "experiment_jobs/" + experiment + "/" + "combined_jobs/" + experiment + "_" + str(i) + ".sh", "w")
+            writeTestShellFile(file)
+            writeTestShellFile(file, experiment, "runCombined_" + str(i))
+            os.system("chmod +x " + PATH + "experiment_jobs/" + experiment + "/" + "combined_jobs/" + experiment + "_" + str(i) + ".sh")
+
+            
+            if i != csnumfs-1:
+                file2.write("qsub " + PATH + "experiment_jobs/" + experiment + "/" + "combined_jobs/" + experiment + "_" + str(i) + ".sh" + "\n")
+            else:
+                # file2.write("wait" + "\n")
+                file2.write("qsub " + PATH + "experiment_jobs/" + experiment + "/" + "combined_jobs/" + experiment + "_" + str(i) + ".sh" + "\n")
+
+        file2.close()        
+        
+        os.system("chmod +x " + PATH + "submit_jobs/" + experiment + "/" + "seperate_jobs.sh")
+        
+        os.system("chmod +x " + PATH + "submit_jobs/" + experiment + "/" + "combined_jobs.sh")
+        
+        file3 = open(PATH + experiment + ".sh", "w")
+        file3.write("./submit_jobs/" + experiment + "/" + "seperate_jobs.sh" + "\n")
+        
+        
+        
+        file3.write("./submit_jobs/" + experiment + "/" + "combined_jobs.sh" + "\n")
+        file3.close()
         
         
         
