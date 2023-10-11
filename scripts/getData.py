bfed4cb8ad7af2cfcf60033c2b208fe04d0aa8e5
Manuel Marte <mjmarte@scc1.bu.edu>
2023-10-11 15:27:56 -0400

    Reads the data and sends it to training

--- scripts/getData.py
+++ scripts/getData.py
@@ -22,21 +22,19 @@ class getData:
         if dta == "RS": # Done
             print("loading resting state data")
             self.init()
-        elif dta == "stan_optimal": 
-            print("loading stan's data")
-            self.stan_optimal()
+
+        # elif dta == "stan_optimal": 
+        #     print("loading stan's data")
+        #     self.stan_optimal()
             
-        elif dta == "combined_features":
-            self.combined_data()
-        # elif dta == "LS": # Done
-        #     print("loading WM GM lesion remaining data")
-            # self.init2()
-        # elif dta == "MM": # Done
-        #     self.multi_modal()
+        # elif dta == "combined_features":
+        #     self.combined_data()
+            
         else:
             self.dta = dta
             self.init3()
         
+
     def readRSData(self):
         self.init()
         self.correlation_data = pd.DataFrame(self.correlation_data, columns = list(self.all_features))
@@ -48,14 +46,19 @@ class getData:
         df = pd.read_excel("/projectnb/skiran/saurav/Fall-2022/RS" + "/compiled_dataset_RSbivariate_without_controls_v7.xlsx", header = [0,1])
         outputs = df[("behavioral", "wab_aq_bd")].values
         outputs = outputs.reshape(len(outputs),1)
+        self.participants_ = list(df[("participant", "participant")].values)
+
 
         data = {}
 
         # AQ - Aphasia Quotient
         data["AQ"] = pd.DataFrame(df[('behavioral', "wab_aq_bd")].values, columns = ["wab_aq_bd"]) 
+        # data["AQ"].index = participants
 
         # DM - Demographics
         data["DM"] = df["demographic_info"]
+        # data["DM"].index = participants
+        # data["DM"] = data["DM"].drop(["level_0"], axis = 1)
         
         # print("demographic data shape = ", data["DM"].shape)
 
@@ -65,72 +68,101 @@ class getData:
         FA_L = df["average_FA_values"][featuresFAL].fillna(0).values
         FA_R = df["average_FA_values"][featuresFAR].fillna(df["average_FA_values"][featuresFAR].mean()).values
         data["FA"] = pd.DataFrame(np.hstack((FA_L, FA_R)), columns = featuresFAL+featuresFAR)
+        # data["FA"].index = participants
+        # data["FA"] = data["FA"].drop(["level_0"], axis = 1)
 
         # PS_G - percent grey matter per region  
         data["PSG"] = df["percent_spared_in_gray_matter"]
+        # data["PSG"].index = participants
+        # data["PSG"] = data["PSG"].drop(["level_0"], axis = 1)
 
         # PS_W - percent white matter per region  
         data["PSW"] = df["percent_spared_in_white_matter"]
+        # data["PSW"].index = participants
+        # data["PSW"] = data["PSW"].drop(["level_0"], axis = 1)
 
         # LS - Lesion size as a single metric 
         data["LS"] = pd.DataFrame(df[("lesion_size", "lesion_size_ls")].values, columns = ["lesion_size_ls"])
+        # data["LS"].index = participants
+        # data["LS"] = data["LS"].drop(["level_0"], axis = 1)
 
         # RS - Resting state data
         data["RS"] = self.readRSData()
+        # data["RS"].index = participants
+        # data["RS"] = data["RS"].drop(["level_0"], axis = 1)
 
-        self.all_features = []
-        self.stan_optimal()
-        print("all features len = ", len(self.all_features))
-        data["stan"] = pd.DataFrame(self.correlation_data, columns = list(self.all_features)) 
-        
-        # LS - Lesion size (Total)   ; Need to add lesion size per region, Deal with LS feature later.
+        RS_between = pd.read_csv("/projectnb/skiran/saurav/Fall-2022/src2/data/" + "betweenness.csv")
+        # RS_between.index = participants#RS_between.iloc[:,0]
+        RS_between = RS_between.iloc[:,1:]
+        data["RS-bet"] = RS_between
+        # data["RS-bet"] = data["RS-bet"].drop(["level_0"], axis = 1)
+
+        RS_degree = pd.read_csv("/projectnb/skiran/saurav/Fall-2022/src2/data/" + "degree.csv")
+        # RS_degree.index = participants#RS_degree.iloc[:,0]
+        RS_degree = RS_degree.iloc[:,1:]
+        data["RS-deg"] = RS_degree
+        # data["RS-deg"] = data["RS-deg"].drop(["level_0"], axis = 1)
+
+        RS_eff = pd.read_csv("/projectnb/skiran/saurav/Fall-2022/src2/data/" + "local_efficiency.csv")
+        # RS_eff.index = participants#RS_eff.iloc[:,0]
+        RS_eff = RS_eff.iloc[:,1:]
+        data["RS-eff"] = RS_eff
+        # data["RS-eff"] = data["RS-eff"].drop(["level_0"], axis = 1)
+
+        RS_trans = pd.read_csv("/projectnb/skiran/saurav/Fall-2022/src2/data/" + "local_transitivity.csv")
+        # RS_trans.index = participants#RS_trans.iloc[:,0]
+        RS_trans = RS_trans.iloc[:,1:]
+        data["RS-trans"] = RS_trans
+        # data["RS-trans"] = data["RS-trans"].drop(["level_0"], axis = 1)
 
         self.datadict = data 
         self.outputs = outputs
-        self.all_features = []
-        
+
     
     
-    def combined_data(self):
-        self.read_data()
-        corr_data = []
-        self.all_features = []
-        for i,dataSource in enumerate(list(self.datadict)):
-            # print("dataSource now = ", dataSource)
-            # print("data shape = ", self.datadict[dataSource].shape)
-            if dataSource in datasets:
-                # print("dataSource added = ", dataSource)
-                if len(corr_data) == 0:
-                    corr_data = self.datadict[dataSource].values
-                    self.all_features += list(self.datadict[dataSource].columns)
-                else:
-                    corr_data = np.hstack((corr_data, self.datadict[dataSource].values))
-                    self.all_features += list(self.datadict[dataSource].columns)
+    # def combined_data(self):
+    #     self.read_data()
+    #     corr_data = []
+    #     self.all_features = []
+    #     for i,dataSource in enumerate(list(self.datadict)):
+    #         # print("dataSource now = ", dataSource)
+    #         # print("data shape = ", self.datadict[dataSource].shape)
+    #         if dataSource in datasets:
+    #             # print("dataSource added = ", dataSource)
+    #             if len(corr_data) == 0:
+    #                 corr_data = self.datadict[dataSource].values
+    #                 self.all_features += list(self.datadict[dataSource].columns)
+    #             else:
+    #                 corr_data = np.hstack((corr_data, self.datadict[dataSource].values))
+    #                 self.all_features += list(self.datadict[dataSource].columns)
         
-        print("----")
-        self.correlation_data = corr_data
+    #     # print("----")
+    #     self.correlation_data = corr_data
         
     
     
     def LF_level1(self):
         self.read_data()
-        features = self.dta.split("-")
+        features = self.dta.split("+")
         correlations = []
         for i,dataset in enumerate(features):
             if i==0:
-                correlations = self.datadict[dataset].values
-                featureList = list(self.datadict[dataset].columns)
+                correlations = self.datadict[dataset]
+                # featureList = list(self.datadict[dataset].columns)
             else:
-                correlations = np.hstack((correlations, self.datadict[dataset].values))
-                featureList += list(self.datadict[dataset].columns)
+                correlations = pd.concat([correlations, self.datadict[dataset]], axis = 1)
+                # correlations = np.hstack((correlations, self.datadict[dataset].values))
+                # featureList += list(self.datadict[dataset].columns)
         self.correlation_data = correlations
-        self.all_features = featureList
+        self.all_features = list(self.correlation_data.columns)
+        # self.all_features = featureList
         
     
     def LF_level2(self):
         print("data = ", str(self.dta))
-        data_combination = self.dta.split("_")[0]
-        data_combination = data_combination.split("-")
+        data_combination = self.dta.split("+")
+        data_combination = sorted(data_combination)
+        # data_combination = data_combination.split("-")
         dataPath = "/projectnb/skiran/saurav/Fall-2022/src2/data/" + self.experiment + "/" + "lateFusionData/"
         fname = None
         
@@ -152,12 +184,14 @@ class getData:
         #     data_combination = self.model
         #     fname = data_combination + "_allModalityOutputs.xlsx"
         else:
+            
+            # print("dc = ",data_combination)
 
             data_combination = "_".join(data_combination)
             fname =  self.model + "_" + data_combination + "_ModalityOutputs.xlsx"
         
         
-            
+        print("fileName = ", fname)
         
         dataPath += self.model + "/"  + fname
         
@@ -165,7 +199,7 @@ class getData:
         self.outputs = self.correlation_data["ground truth score"].values
         self.all_features = list(self.correlation_data.columns) 
         self.correlation_data = self.correlation_data.drop(["ground truth score", "Unnamed: 0"], axis = 1)
-        self.correlation_data = self.correlation_data.values
+        # self.correlation_data = self.correlation_data.values
         
 
     def getReshapedData(self, dataSources, dataSizes=None):
@@ -230,43 +264,116 @@ class getData:
 
 
 
+    # def EF_level2_train_data(self):
+    #     files = datasets
+
+    #     if "without-stans-features" in self.experiment or "noStan" in self.experiment:
+    #         if "stan_optimal" in datasets:
+    #             files.remove("stan_optimal")
+    #     final_path = "/projectnb/skiran/saurav/Fall-2022/src2/results/"
+    #     dataSources = [f.split("_")[0] + "_results" for f in files if not os.path.isfile(final_path + f)] 
+    #     self.dataFeatures = dict()
+        
+        
+    #     for dataSource in dataSources:
+    #         path = PATH + "results/" + self.experiment + "/" + self.approach + "/" + self.model + "_predictions" + "/" + "level1" + "/" + dataSource + "/" + "bestFeatureSet.csv"
+    #         data = pd.read_csv(path)
+    #         self.dataFeatures[dataSource.split("_")[0]] = list(data["features"])
+        
+        
+    #     self.getReshapedData(dataSources)
+    #     # print("dataSizes = ", dataSizes, dataSources)
+
+    #     data_combination = self.dta.split("_")[0]
+    #     data_combination = data_combination.split("-")
+        
+    #     # print("data combination = ", data_combination)
+    #     final_data = None
+    #     for i,dataSource in enumerate(data_combination):
+    #         if i==0:
+    #             final_data = self.datadict[dataSource]
+    #             # print("dataSource = ", dataSource)
+    #             # print("data shape here = ", final_data.shape)
+    #             self.all_features += list(self.datadict[dataSource].columns)
+    #         else:
+    #             final_data = np.hstack((final_data, self.datadict[dataSource]))
+    #             # print("dataSoure = ", dataSource)
+    #             self.all_features += list(self.datadict[dataSource].columns)
+        
+    #     self.correlation_data = final_data
+
+
     def EF_level2_train_data(self):
-        files = datasets
+        
+        data_combination = self.dta.split("+")
+        data_combination = sorted(data_combination)
+        
+        final_path = "/projectnb/skiran/saurav/Fall-2022/src2/results/" + self.experiment + "/EF/" + self.model + "_predictions" + "/" + "level1/"
+        
+        nfolds = 12 if "normal" in self.experiment else 56
+        
+        
+        
+        self.read_data()
 
-        if "without-stans-features" in self.experiment or "noStan" in self.experiment:
-            if "stan_optimal" in datasets:
-                files.remove("stan_optimal")
-        final_path = "/projectnb/skiran/saurav/Fall-2022/src2/results/"
-        dataSources = [f.split("_")[0] + "_results" for f in files if not os.path.isfile(final_path + f)] 
-        self.dataFeatures = dict()
+        self.all_features = []        
+        for fold in range(1,nfolds,1):
+            foldFeatures = []    
+            for dataset in data_combination:
+                fpath = final_path + dataset + "_results" + "/fold-" + str(fold) + "/" + "bestFeatureSet.csv"
+                
+                featureDf = pd.read_csv(fpath)
+                flist = list(featureDf["features"].values)
+                foldFeatures += flist 
+            self.all_features.append(foldFeatures)
+            
         
         
-        for dataSource in dataSources:
-            path = PATH + "results/" + self.experiment + "/" + self.approach + "/" + self.model + "_predictions" + "/" + "level1" + "/" + dataSource + "/" + "bestFeatureSet.csv"
-            data = pd.read_csv(path)
-            self.dataFeatures[dataSource.split("_")[0]] = list(data["features"])
-        
-        
-        self.getReshapedData(dataSources)
-        # print("dataSizes = ", dataSizes, dataSources)
+        # df2 = [self.datadict[key] for key in data_combination]
+        # result = pd.concat(df2, axis=1)
 
-        data_combination = self.dta.split("_")[0]
-        data_combination = data_combination.split("-")
         
-        # print("data combination = ", data_combination)
-        final_data = None
-        for i,dataSource in enumerate(data_combination):
-            if i==0:
-                final_data = self.datadict[dataSource]
-                # print("dataSource = ", dataSource)
-                # print("data shape here = ", final_data.shape)
-                self.all_features += list(self.datadict[dataSource].columns)
+        # result = pd.concat(selected_dataframes, axis=1)
+        df2 = []
+        for dataset in data_combination:
+            
+            if len(df2) == 0:
+                df2 = self.datadict[dataset]
+                df2 = df2.reset_index()
+                # print("datadict shape before = ", df2.shape)
             else:
-                final_data = np.hstack((final_data, self.datadict[dataSource]))
-                # print("dataSoure = ", dataSource)
-                self.all_features += list(self.datadict[dataSource].columns)
+                # df2 = pd.hstack((df2, self.datadict[dataset]))
+                A = self.datadict[dataset].reset_index()
+                df2 = pd.concat([df2, A], axis=1)
+                # df2 = df2.reset_index()
+                # df2 = df2.join(self.datadict[dataset])
+        print("datasets = ", data_combination)
+        print("data shape = ", df2.shape)
+        df2 = df2.fillna(df2.median())
+
+        print("total dfolds = ", len(self.all_features))
+        nanvalues = df2.isna().sum().sum()
+        print("nan value count = ", nanvalues)
+
+        for i, features in enumerate(self.all_features):
+            print("shape_" + str(i) + " = ", df2[features].shape)
+
+        # "+".join(datasets).to(csv(""))
+            
+
+        if "level_0" in list(df2.columns):
+            df2 = df2.drop(["level_0"], axis = 1)
+
+        df2.to_csv("/projectnb/skiran/saurav/Fall-2022/src2/experiment_jobs/EXP-final-SVR/combined_jobs/" + "_".join(data_combination)+".csv")
+        print("\n\n")
+        # print("all features")
+        # print(self.all_features)
+                
+        self.correlation_data= df2
+            
+                
         
-        self.correlation_data = final_data
+
             
     
     def EF_level2(self):
@@ -463,6 +570,7 @@ class getData:
                 self.all_features = df["index"]
 
             self.correlation_data = np.array(self.correlation_data)
+            self.correlation_data = pd.DataFrame(self.correlation_data, columns = list(self.all_features))
             self.outputs =  np.array(self.outputs)
 
         # for patient_index, patient_file in enumerate(self.files):
