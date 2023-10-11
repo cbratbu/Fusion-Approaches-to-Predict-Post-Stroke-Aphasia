029ff62c503b94bf0cabc4a5ac006e998eeea019
Manuel Marte <mjmarte@scc1.bu.edu>
2023-10-11 16:12:04 -0400

    LSF scripts for all experiments

---
+++ scripts/test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -l
+
+# Set SCC project
+$ -P projectnb/skiran/saurav/Fall-2022/src2/
+
+# Request 28 CPUs
+#$ -pe omp 28
+#$ -l mem_per_core=1.2G
+
+#$ -l h_rt=70:00:00
+#$ -m beas
+
+module load python3/3.8.10
+module load pytorch/1.11.0
+
+python3 EachFold.py
